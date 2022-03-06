505f7d35649c95e0e7a2cfe85ce83700bc0385a9
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2022-03-07 01:11:42 +0400

    Global Error Handling

---
+++ bot/cefsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SharpBrowser
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            //Console.WriteLine("MyHandler caught : " + e.Message);
+        }
+    }
+}
