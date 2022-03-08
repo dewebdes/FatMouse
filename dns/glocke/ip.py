54ec120ebb74b05885d04e1233f5a60795c86162
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2022-03-09 02:40:03 +0400

    Update ip.py

--- dns/glocke/ip.py
+++ dns/glocke/ip.py
@@ -20,22 +20,42 @@ def parse_output(outp):
 		myfile = open("uphosts.txt","a")
 		myfile.write(str(ip) + '\n')
 		myfile.close()
+		myfile = open("banners.txt","a")
+		myfile.write(str(host) + '\n')
+		myfile.close()
 
 f = open("ipdb.txt", "r")
 fc = f.read().splitlines()
+f.close()
+f = open("uphosts.txt", "r")
+hlines = f.read().splitlines()
+doscan = True
+lasthost = ''
+if len(hlines) > 0:
+	lasthost = hlines[len(hlines) - 1]
+	doscan = False
+f.close()
+	  
 for ln in fc:
 	ipl = ln.split(',')
 	for ir in ipl:
 		if ir:
 			if ':' in ir:
 				for ip in ipaddress.IPv6Network(unicode(ir)):
-					#myfile.write(str(ip) + '\n')
-					cmd = "nmap -6 -sP " + str(ip)
-					output = run_command(cmd)
-					parse_output(output)
+					if doscan:
+						cmd = "nmap -6 -sP " + str(ip)
+						output = run_command(cmd)
+						parse_output(output)
+					else:
+						if str(ip) == lasthost:
+							doscan = True
 			else:
 				for ip in ipaddress.IPv4Network(unicode(ir)):
-					#myfile.write(str(ip) + '\n')
-					cmd = "nmap -sP " + str(ip)
-					output = run_command(cmd)
-					parse_output(output)
+					if doscan:
+						cmd = "nmap -sP " + str(ip)
+						output = run_command(cmd)
+						parse_output(output)
+					else:
+						if str(ip) == lasthost:
+							doscan = True
+
